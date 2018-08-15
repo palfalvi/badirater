51fd86137fad22ad9092bd8c5da02da20b7b3192
Gergo Palfalvi <palfalvi.gergo@gmail.com>
2018-08-15 18:56:46 +0900

    include first test

--- tests/testthat/test_something.R
+++ tests/testthat/test_something.R
@@ -1,6 +1,6 @@
 context("First test")
 library(badirater)
 
-test_that("str_length is number of characters", {
-  expect_
+test_that("test tree decoding", {
+  expect_equal(nrow(bd_decode_tree_ids(tree_id = "branch_ids.tree")), 23, info = "Tree decoding")
 })
